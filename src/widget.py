3f7ec78bc2dc7a14f3ec2d419751b481d9c322cd
Natalya Sharova <nat_sha10@mail.ru>
2025-08-15 00:36:44 +0300

    add widget with functions

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from masks import get_mask_account, get_mask_card
+
+
+def mask_account_card(account_card: str) -> str:
+    """Принимает тип и номер карты или счета, возвращает строку с замаскированным номером"""
+    acc_card = account_card.split()
+    if 'Счет' in account_card:
+        mask = get_mask_account(acc_card[-1])
+    else:
+        mask = get_mask_card(acc_card[-1])
+    return " ".join(acc_card[0:-1]) + " " + mask
+
+
+def get_date(date: str) -> str:
+    """Форматирует дату в 'ДД.ММ.ГГГГ'"""
+    return date[8:10] + "." + date[5:7] + "." + date[:4]
