2a90813901dd0e0485a5efb9086578b7b48ae1bf
Natalya Sharova <nat_sha10@mail.ru>
2025-08-16 11:41:08 +0300

    fixed bugs

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,16 @@
-from masks import get_mask_account, get_mask_card
+from masks import get_mask_account
+from masks import get_mask_card
 
 
 def mask_account_card(account_card: str) -> str:
-    """Принимает тип и номер карты или счета, возвращает строку с замаскированным номером"""
+    """Принимает тип и номер карты или счета,
+    возвращает строку с замаскированным номером"""
     acc_card = account_card.split()
     if "Счет" in account_card:
         mask = get_mask_account(acc_card[-1])
     else:
         mask = get_mask_card(acc_card[-1])
-    return " ".join(acc_card[0:-1]) + " " + mask
+    return str(" ".join(acc_card[0:-1]) + " " + mask)
 
 
 def get_date(date: str) -> str:
