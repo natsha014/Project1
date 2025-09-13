b69d3f3d3ecc50b01f81983fb4742011de4fe4ba
Natalya Sharova <nat_sha10@mail.ru>
2025-09-13 21:28:21 +0300

    add tests and fixed funcs and README

--- src/masks.py
+++ src/masks.py
@@ -2,13 +2,17 @@ def get_mask_card(card_number: int | str) -> str:
     """Принимает номер карты и возвращает маску в виде ХХХХ ХХ** **** ХХХХ"""
 
     card = str(card_number)
-
-    return card[:4] + " " + card[4:6] + "** **** " + card[12:]
+    if len(card) == 16 and card.isdigit():
+        return card[:4] + " " + card[4:6] + "** **** " + card[12:]
+    else:
+        return "Некорректный ввод"
 
 
 def get_mask_account(account_number: int | str) -> str:
     """Принимает номер счета и возвращает его маску в виде **ХХХХ"""
 
     a = str(account_number)
-
-    return "**" + a[-4:]
+    if len(a) == 20 and a.isdigit():
+        return "**" + a[-4:]
+    else:
+        return "Некорректный ввод"
